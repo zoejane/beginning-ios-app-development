0a85f62bb177b7022897360dc99488ba61e1c161
Zoe <dadac123@gmail.com>
2016-10-20 12:47:59 +0800

    add practice of dictionary

--- ios-nd-beginning-ios-swift-3/Playground Collection/Part 4 - Alien Adventure 2/Advanced Loops/Advanced Loops.playground/Pages/Checkerboard Final.xcplaygroundpage/Contents.swift
+++ ios-nd-beginning-ios-swift-3/Playground Collection/Part 4 - Alien Adventure 2/Advanced Loops/Advanced Loops.playground/Pages/Checkerboard Final.xcplaygroundpage/Contents.swift
@@ -5,7 +5,9 @@
 print("The final answer is below!")
 for i in 1...8 {
     for j in 1...8 {
-        if j % 2 == i % 2 {
+        if j % 4 == 0 && i % 4 == 0{
+            print("🆒", terminator: "")
+        }else if j % 2 == i % 2 {
             print("⬛", terminator: "")
         } else {
             print("◻️", terminator: "")
